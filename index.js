ad18b560d2e563f0986af7bf0d7dea80287f0e79
Johannes-Kostas Goetzinger <goetzinger@mediapeers.com>
2020-03-26 20:13:17 +0100

    So my boss wanted this button ...

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 const config = {
   suffix: 'T1QSP9Y1J/B5V1AM1C6/bUq4VSMqa5yrHYZRzNYFMoTK',
-  mpx_production: {
-    env: 'production',
-    aws_profile: 'production',
-    branch: 'legacy',
-    endpoints: {
-      tuco: "https://tuco.api.mediapeers.biz",
-      pigeon: "https://pigeon.api.mediapeers.biz",
-      um: "https://um.api.mediapeers.biz/v20140601",
-      pm: "https://pm.api.mediapeers.biz/v20140601",
-      am: "https://am.api.mediapeers.biz/v20140601",
-      ac: "https://ac.api.mediapeers.biz/v20140601",
-      sm: "https://sm.api.mediapeers.biz/v20140601",
-      mc: "https://mc.api.mediapeers.biz/v20140601",
+  envs: {
+    mpx_production: {
+      env: 'production',
+      aws_profile: 'production',
+      branch: 'legacy',
+      endpoints: {
+        tuco: "https://tuco.api.mediapeers.biz",
+        pigeon: "https://pigeon.api.mediapeers.biz",
+        um: "https://um.api.mediapeers.biz/v20140601",
+        pm: "https://pm.api.mediapeers.biz/v20140601",
+        am: "https://am.api.mediapeers.biz/v20140601",
+        ac: "https://ac.api.mediapeers.biz/v20140601",
+        sm: "https://sm.api.mediapeers.biz/v20140601",
+        mc: "https://mc.api.mediapeers.biz/v20140601",
+      },
+      'mpx-ui-gcui': {
+        'base2': {
+          bucket: 'mpx-v2-production-buyer-fe-mpx',
+          distribution_ids: ['E3OVGR028R78SH'],
+        }
+      },
     },
-    'mpx-ui-gcui': {
-      'base2': {
-        bucket: 'mpx-v2-production-buyer-fe-mpx',
-        distribution_ids: ['E3OVGR028R78SH'],
-      }
-    },
-  },
-  mpx_staging: {
-    env: 'staging',
-    aws_profile: 'staging',
-    branch: 'staging',
-    endpoints: {
-      tuco: "https://tuco.api.mediapeers.mobi",
-      pigeon: "https://pigeon.api.mediapeers.mobi",
-      um: "https://um.api.mediapeers.mobi/v20140601",
-      pm: "https://pm.api.mediapeers.mobi/v20140601",
-      am: "https://am.api.mediapeers.mobi/v20140601",
-      ac: "https://ac.api.mediapeers.mobi/v20140601",
-      sm: "https://sm.api.mediapeers.mobi/v20140601",
-      mc: "https://mc.api.mediapeers.mobi/v20140601",
-    },
-    'mpx-ui-gcui': {
-      'base2': {
-        bucket: 'mpx-v2-staging-buyer-fe-mpx',
-        distribution_ids: ['E13JQIPFDFHOUI'],
-      }
+    mpx_staging: {
+      env: 'staging',
+      aws_profile: 'staging',
+      branch: 'staging',
+      endpoints: {
+        tuco: "https://tuco.api.mediapeers.mobi",
+        pigeon: "https://pigeon.api.mediapeers.mobi",
+        um: "https://um.api.mediapeers.mobi/v20140601",
+        pm: "https://pm.api.mediapeers.mobi/v20140601",
+        am: "https://am.api.mediapeers.mobi/v20140601",
+        ac: "https://ac.api.mediapeers.mobi/v20140601",
+        sm: "https://sm.api.mediapeers.mobi/v20140601",
+        mc: "https://mc.api.mediapeers.mobi/v20140601",
+      },
+      'mpx-ui-gcui': {
+        'base2': {
+          bucket: 'mpx-v2-staging-buyer-fe-mpx',
+          distribution_ids: ['E13JQIPFDFHOUI'],
+        }
+      },
     },
   },
 }
