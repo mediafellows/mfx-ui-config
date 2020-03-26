da58670c01748ac82b1fb00849a20693a65d6c33
Johannes-Kostas Goetzinger <goetzinger@mediapeers.com>
2020-03-26 15:44:23 +0100

    adds new legacy production config for GCUI

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var config = {
-  'mpx-ui-gcui': {
-    environments: {
-      'legacy_production': {
-        env: 'production',
-        aws_profile: 'production',
-        endpoints: {
-          tuco: "https://tuco.api.mediapeers.biz",
-          pigeon: "https://pigeon.api.mediapeers.biz",
-          um: "https://um.api.mediapeers.biz/v20140601",
-          pm: "https://pm.api.mediapeers.biz/v20140601",
-          am: "https://am.api.mediapeers.biz/v20140601",
-          ac: "https://ac.api.mediapeers.biz/v20140601",
-          sm: "https://sm.api.mediapeers.biz/v20140601",
-          mc: "https://mc.api.mediapeers.biz/v20140601"
-        },
-        sites: {
-          'generic': {
+  environments: {
+    mpx_production: {
+      aws_profile: 'production',
+      default_branch: 'legacy',
+      endpoints: {
+        tuco: "https://tuco.api.mediapeers.biz",
+        pigeon: "https://pigeon.api.mediapeers.biz",
+        um: "https://um.api.mediapeers.biz/v20140601",
+        pm: "https://pm.api.mediapeers.biz/v20140601",
+        am: "https://am.api.mediapeers.biz/v20140601",
+        ac: "https://ac.api.mediapeers.biz/v20140601",
+        sm: "https://sm.api.mediapeers.biz/v20140601",
+        mc: "https://mc.api.mediapeers.biz/v20140601",
+      },
+      frontends: {
+        'mpx-ui-gcui': {
+          'base2': {
             bucket: 'mpx-v2-production-buyer-fe-mpx',
             distribution_ids: ['E3OVGR028R78SH'],
           }
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 }
 
 module.exports = config
